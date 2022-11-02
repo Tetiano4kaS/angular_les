19d7b23b09a2f1acfd29355302ac8260a13ce636
Tetiano4kaS <stecko332@gmail.com>
2022-11-02 11:57:21 +0200

    Add lesson_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { UsersComponent } from './components/users/users.component';
-import { UserComponent } from './components/user/user.component';
+import {AppComponent} from './app.component';
+import {UsersComponent} from './components/users/users.component';
+import {UserComponent} from './components/user/user.component';
 import {HttpClientModule} from "@angular/common/http";
-import { PostComponent } from './components/post/post.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { CommentComponent } from './components/comment/comment.component';
-import { CommentsComponent } from './components/comments/comments.component';
+import {PostComponent} from './components/post/post.component';
+import {PostsComponent} from './components/posts/posts.component';
+import {CommentComponent} from './components/comment/comment.component';
+import {CommentsComponent} from './components/comments/comments.component';
+import {RouterModule, Routes} from "@angular/router";
+import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
+import {HeaderComponent} from './components/header/header.component';
+import { UserDetailsComponent } from './components/user.details/user.details.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: 'users', pathMatch: 'full'},
+      {path: 'users', component: UsersComponent, children: [{path:':id', component:UserDetailsComponent}]},
+      {path: 'posts', component: PostsComponent},
+      {path: 'comments', component: CommentsComponent}]
+  },
+];
 
 @NgModule({
   declarations: [
@@ -18,13 +33,18 @@ import { CommentsComponent } from './components/comments/comments.component';
     PostComponent,
     PostsComponent,
     CommentComponent,
-    CommentsComponent
+    CommentsComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {IUser} from "../../interfaces";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-user',
@@ -8,11 +9,22 @@ import {IUser} from "../../interfaces";
 })
 export class UserComponent implements OnInit {
   @Input()
-user:IUser;
+  user: IUser;
 
-  constructor() { }
+  @Output()
+  liftUser = new EventEmitter<IUser>()
+
+  constructor(private router:Router, private activatedRoute:ActivatedRoute) {
+  }
 
   ngOnInit(): void {
   }
 
+  lift(): void {
+    this.liftUser.emit(this.user)
+  }
+
+  getDetails():void {
+    this.router.navigate([this.user.id], {relativeTo:this.activatedRoute, state:{user:this.user}})
+  }
 }
